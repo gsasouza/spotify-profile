5314391dac71328582f781b5ac7d838e8f554bf9
Arthur Tavares <tavares593@gmail.com>
2017-09-11 20:35:59 -0300

    Some fixes and adaptations on the App.js and a New Index.html file

--- app/app.js
+++ app/app.js
@@ -9,8 +9,6 @@ const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
 
-//const optionsX = require('./options');
-
 const client_id = '1b83fcce67524a858a05fec2bfff28f9';
 const client_secret = '6119312c41554d5e8a3d6c1f6e81c6e7';
 const redirect_uri = 'http://localhost:8080/callback';
@@ -54,15 +52,6 @@ app.get('/login', function(req, res) {
     }));
 });
 
-let artJ = {
-  'artistas':{'recentes':{},
-              'meio':{},
-              'geral':{}},
-  'musicas':{'recentes':{},
-              'meio':{},
-              'geral':{}},
-  'ultimatocada':''
-};
 app.get('/callback', function(req, res) {
   const data = {
     artists: {
@@ -79,7 +68,7 @@ app.get('/callback', function(req, res) {
   }
   const code = req.query.code || null;
   const state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state === null || state !== storedState) {
     return res.redirect('/#' +
@@ -113,30 +102,33 @@ app.get('/callback', function(req, res) {
           url: url
         }
       };
-      function formatData(data, index){
+      function formatDataArtists(data, index){
         return `${index + 1}. ${data.name}`;
+      }
+      function formatDataTracks(data,index){
+        return `${index + 1}. ${data.artists[0].name} - ${data.name}`
       }
       axios(makeOptions('https://api.spotify.com/v1/me/top/artists?time_range=short_term'))
         .then((result)=> {
-          data.artists.recents = result.data.items.map(formatData);
+          data.artists.recents = result.data.items.map(formatDataArtists);
           return axios(makeOptions('https://api.spotify.com/v1/me/top/artists?time_range=medium_term'))
         }).then((result)=> {
-          data.artists.middle = result.data.items.map(formatData);;
+          data.artists.middle = result.data.items.map(formatDataArtists);
           return axios(makeOptions('https://api.spotify.com/v1/me/top/artists?time_range=long_term'))
         }).then((result)=> {
-          data.artists.general = result.data.items.map(formatData);;
+          data.artists.general = result.data.items.map(formatDataArtists);
           return axios(makeOptions('https://api.spotify.com/v1/me/top/tracks?time_range=short_term'))
         }).then((result)=> {
-          data.tracks.recents = result.data.items.map(formatData);;
+          data.tracks.recents = result.data.items.map(formatDataTracks);
           return axios(makeOptions('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term'))
         }).then((result)=> {
-          data.tracks.middle = result.data.items.map(formatData);;
+          data.tracks.middle = result.data.items.map(formatDataTracks);
           return axios(makeOptions('https://api.spotify.com/v1/me/top/tracks?time_range=long_term'))
         }).then((result)=>{
-          data.tracks.general = result.data.items.map(formatData);;
+          data.tracks.general = result.data.items.map(formatDataTracks);
           return axios(makeOptions('https://api.spotify.com/v1/me/player/recently-played?limit=10'))
         }).then((result)=>{
-          data.lastMusic = result.data.items.map((track, index)=> `${index + 1}. ${track.track.name}`);;
+          data.lastMusic = result.data.items.map((track, index)=> `${index + 1}. ${track.track.artists[0].name} - ${track.track.name}`);
           return res.send(data);
         }).catch((error)=> res.send(error));
     }
@@ -144,36 +136,6 @@ app.get('/callback', function(req, res) {
   });
 
 });
-
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
-  });
-});
-
-
-app.get('/data', (req, res) => {
-  console.log('information sent.'); //debug
-  res.send(artJ);
-})
 
 console.log('Listening on 8080');
 app.listen(8080);
